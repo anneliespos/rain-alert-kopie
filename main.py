09b82cd55591c6cb1ce411bc095a5c9d55712573
anneliespos <anneliespos@gmail.com>
2025-01-22 20:28:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from twilio.rest import Client
 
-MY_LAT = 51.507351
-MY_LONG = -0.127758
-api_key = "4303269376d8edc3cff3e113700d453f"
+MY_LAT = 51.507351      #example in London
+MY_LONG = -0.127758     #example in London
+api_key = "your api key"
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 account_sid = "account sid from site"
 auth_token = "your auth token from site"
@@ -18,7 +18,6 @@ weather_parameters = {
 response = requests.get(OWM_ENDPOINT, params=weather_parameters)
 response.raise_for_status()
 weather_data = response.json()
-# print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
 for hour_data in weather_data["list"]:
@@ -31,8 +30,8 @@ if will_rain:
     message = client.messages \
         .create(
         body="Bring an umbrella",
-        from= "+15123456789",        #new phone number you got from twilio
-        to= "+15987654321"
+        from= "twilio phone number",        #new phone number you got from twilio
+        to= "your own phone number"
     )      #your own verifier phone number (that you signed up with)
 
     print(message.status)
